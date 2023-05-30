5710349890f986e180820f467db16a3ca33273ab
1129573032@qq.com <1129573032@qq.com>
2023-05-30 17:45:17 +0800

    fixed failing tests

--- src/components/Block/Block.test.tsx
+++ src/components/Block/Block.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event'
-import { screen } from '@testing-library/dom'
 
 import Block from './';
 
--- src/components/BlockChain/BlockChain.test.tsx
+++ src/components/BlockChain/BlockChain.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event'
 
 import BlockChain from './';
@@ -11,61 +11,61 @@ import BlockChain from './';
  */
 
 it('Block chain should be empty', () => {
-  const { getByText } = render(<BlockChain />);
-  expect(getByText("Total Blocks: 0")).toBeInTheDocument();
+  render(<BlockChain />);
+  expect(screen.getByText("Total Blocks: 0")).toBeInTheDocument();
 });
 
 it('Add block works correctly', () => {
-  const { getByText } = render(<BlockChain />);
-  userEvent.click(getByText('Add Block'));
-  expect(getByText("Total Blocks: 1")).toBeInTheDocument();
-  expect(getByText("Not Valid")).toBeInTheDocument();
+  render(<BlockChain />);
+  userEvent.click(screen.getByText('Add Block'));
+  expect(screen.getByText("Total Blocks: 2")).toBeInTheDocument();
+  expect(screen.getByText("Not Valid")).toBeInTheDocument();
 });
 
 it('Multiple blocks works correctly', () => {
-  const { getAllByText, getByText } = render(<BlockChain />);
-  userEvent.click(getByText('Add Block'));
-  userEvent.click(getByText('Add Block'));
-  expect(getByText("Total Blocks: 2")).toBeInTheDocument();
-  expect(getAllByText("Not Valid").length).toBe(2);
+  render(<BlockChain />);
+  userEvent.click(screen.getByText('Add Block'));
+  userEvent.click(screen.getByText('Add Block'));
+  expect(screen.getByText("Total Blocks: 3")).toBeInTheDocument();
+  expect(screen.getAllByText("Not Valid").length).toBe(3);
 });
 
 it('Hash continues from block 1 to block 2', () => {
-  const { getAllByText, getByText } = render(<BlockChain />);
+  render(<BlockChain />);
   // Click twice for 2 blocks
-  userEvent.click(getByText('Add Block'));
-  userEvent.click(getByText('Add Block'));
-  const hash = getAllByText('Hash')[0].children[0].textContent || '';
-  expect(getAllByText(hash).length).toBe(2);
+  userEvent.click(screen.getByText('Add Block'));
+  userEvent.click(screen.getByText('Add Block'));
+  const hash = screen.getAllByText('Hash')[0].children[0].textContent || '';
+  expect(screen.getAllByText(hash).length).toBe(2);
 });
 
 it('Mining multiple blocks works correctly', () => {
-  const { getAllByText, getByText } = render(<BlockChain />);
-  userEvent.click(getByText('Add Block'));
-  userEvent.click(getByText('Add Block'));
+  render(<BlockChain />);
+  userEvent.click(screen.getByText('Add Block'));
+  userEvent.click(screen.getByText('Add Block'));
   // Mine the first block
-  userEvent.click(getAllByText('Mine')[0]);
+  userEvent.click(screen.getAllByText('Mine')[0]);
   // Hash contents of first block
-  const hash1 = getAllByText('Hash')[0].children[0].textContent || '';
+  const hash1 = screen.getAllByText('Hash')[0].children[0].textContent || '';
   expect(hash1.substring(0, 3)).toBe('000');
   // This hash should exists in 2 places, block 1's hash and block 2's previousHash
-  expect(getAllByText(hash1).length).toBe(2);
-  expect(getByText("Not Valid")).toBeInTheDocument();
-  expect(getByText("Valid")).toBeInTheDocument();
+  expect(screen.getAllByText(hash1).length).toBe(2);
+  expect(screen.getByText("Not Valid")).toBeInTheDocument();
+  expect(screen.getByText("Valid")).toBeInTheDocument();
   // Mine the second block
-  userEvent.click(getAllByText('Mine')[1]);
+  userEvent.click(screen.getAllByText('Mine')[1]);
   // Hash contents of second block
-  const hash2 = getAllByText('Hash')[1].children[0].textContent || '';
+  const hash2 = screen.getAllByText('Hash')[1].children[0].textContent || '';
   expect(hash2.substring(0, 3)).toBe('000');
   // Both blocks are now valid
-  expect(getAllByText("Valid").length).toBe(2);
+  expect(screen.getAllByText("Valid").length).toBe(2);
 });
 
 it('On delete works correctly', () => {
-  const { getByText } = render(<BlockChain />);
-  userEvent.click(getByText('Add Block'));
-  userEvent.click(getByText('Add Block'));
-  expect(getByText("Total Blocks: 2")).toBeInTheDocument();
-  userEvent.click(getByText('Delete'));
-  expect(getByText("Total Blocks: 1")).toBeInTheDocument();
+  render(<BlockChain />);
+  userEvent.click(screen.getByText('Add Block'));
+  userEvent.click(screen.getByText('Add Block'));
+  expect(screen.getByText("Total Blocks: 3")).toBeInTheDocument();
+  userEvent.click(screen.getByText('Delete'));
+  expect(screen.getByText("Total Blocks: 2")).toBeInTheDocument();
 });
